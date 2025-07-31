b53e7f61d1e2a071443f40ce7691e45d72bf2e3c
Fikresilase <shitubilbila@gmail.com>
2025-07-31 18:51:02 +0300

    title change

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Redirecting to Input | MyApp</title>
+      <meta name="description" content="Redirecting to the input page." />
+    </>
+  );
+}
