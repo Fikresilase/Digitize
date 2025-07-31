3d36c2cd87d8917e75aeff5dfb083a42e53d49e1
Fikresilase <shitubilbila@gmail.com>
2025-07-31 10:52:15 +0300

    initialized

---
+++ src/app/imput/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImputPage = () => {
+    return (
+        <main>
+            <h1>Imput Page</h1>
+            {/* Add your content here */}
+        </main>
+    );
+};
+
+export default ImputPage;
